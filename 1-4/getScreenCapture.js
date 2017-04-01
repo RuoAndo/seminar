64ec8c52cac5f8249bd08475735c63f62816dffc
Ruo Ando <ando.ruo@gmail.com>
2017-04-01 22:49:01 +0900

    screen capture

---
+++ 1-4/getScreenCapture.js
@@ -0,0 +1,24 @@
+var casper = require('casper').create({
+	pageSettings: {
+	    webSecurityEnabled: false
+	}
+    });
+
+var url = casper.cli.get('url'); 
+
+casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36 ');
+casper.start(url,function(){
+
+    });
+casper.then(function(){
+	this.wait(3000, function() {
+	    });
+    });
+
+casper.then(function () {
+	this.echo( this.getCurrentUrl());
+	casper.capture("nodejs.png");
+    });
+
+casper.run();
+ 
