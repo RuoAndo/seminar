6eec52e390ec2424884f0e5822c6c58a9808f306
Ruo Ando <ando.ruo@gmail.com>
2017-05-20 07:08:21 +0000

    do

--- 1-5/do.sh
+++ 1-5/do.sh
@@ -3,6 +3,6 @@
 TESTFILE=$1
 while read line; do
     #echo $line
-    python abuseip.py $line 2FB6kO3T8KrCAPcHgMm51tO8PjN4JLRcOL4s6Sed 30
+    python abuseip.py $line --TOKEN-- 30
     sleep 1m
 done < $TESTFILE
