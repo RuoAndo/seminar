1a2ba52b0ebd98279ae4ad8a5f027539715c0ef9
Ruo Ando <ando.ruo@gmail.com>
2018-11-09 18:48:23 +0900

    plot

---
+++ jt/3-matplotlib-test.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# Matplotlib example
+
+#matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2 * np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+       title='showing sin wave')
+ax.grid()
+
+plt.show()
