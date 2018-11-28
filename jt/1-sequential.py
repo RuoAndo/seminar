7434ceab0a27cb0212c608bb6945245152166f33
Ruo Ando <ando.ruo@gmail.com>
2018-11-28 15:12:04 +0900

    1

--- jt/1-sequential.py
+++ jt/1-sequential.py
@@ -12,13 +12,13 @@ from keras.utils import np_utils
 #from keras import np_utils
 import numpy as np
 
-data = np.random.random((1000, 784))
-labels = np.random.randint(10, size=(1000, 1))
+data = np.random.random((1024, 512))
+labels = np.random.randint(10, size=(1024, 1))
 labels = np_utils.to_categorical(labels, 10)
 
 model = Sequential()
-model.add(Dense(64, activation='relu', input_dim=784))
-model.add(Dense(64, activation='relu'))
+model.add(Dense(96, activation='relu', input_dim=512))
+model.add(Dense(96, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 
 model.compile(optimizer='rmsprop', 
