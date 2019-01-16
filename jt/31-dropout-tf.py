6a14b56db57c4488792f4f36a4ef536f26be2266
Ruo Ando <ando.ruo@gmail.com>
2019-01-16 14:26:14 +0900

    31

--- jt/31-dropout-tf.py
+++ jt/31-dropout-tf.py
@@ -4,9 +4,6 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
-#from scipy.io import loadmat
-#from six.moves import urllib
-#from sklearn.datasets import fetch_mldata
 from sklearn.datasets.base import get_data_home 
 from sklearn.datasets import fetch_mldata
 
@@ -16,21 +13,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 np.random.seed(0)
 tf.set_random_seed(1234)
 
-#mnist = fetch_mldata('MNIST original', data_home='./mnist_data/')
-#mnist = fetch_mldata('MNIST original')
-#mnist = datasets.fetch_mldata('MNIST original', data_home='
-
-#mnist_raw = loadmat("./mnist_data/mldata/mnist-original.mat")
-#mnist = {
-#    "data": mnist_raw["data"].T,
-#    "target": mnist_raw["label"][0],
-#    "COL_NAMES": ["label", "data"],
-#    "DESCR": "mldata.org dataset: mnist-original",
-#}
-
 print (get_data_home())
-# cp mnist_data/mldata/mnist-original.mat /home/flare/scikit_learn_dat
-#mnist = fetch_mldata('MNIST original')
 mnist = fetch_mldata('MNIST original')
 
 n = len(mnist.data)
@@ -53,28 +36,32 @@ x = tf.placeholder(tf.float32, shape=[None, n_in])
 t = tf.placeholder(tf.float32, shape=[None, n_out])
 keep_prob = tf.placeholder(tf.float32) 
 
+# truncated_normal: Tensorを正規分布かつ標準偏差の２倍までのランダムな値で初期化する
+
 W0 = tf.Variable(tf.truncated_normal([n_in, n_hidden], stddev=0.01))
 b0 = tf.Variable(tf.zeros([n_hidden]))
 h0 = tf.nn.relu(tf.matmul(x, W0) + b0)
 h0_drop = tf.nn.dropout(h0, keep_prob)
 
+# h0_drop
 W1 = tf.Variable(tf.truncated_normal([n_hidden, n_hidden], stddev=0.01))
 b1 = tf.Variable(tf.zeros([n_hidden]))
 h1 = tf.nn.relu(tf.matmul(h0_drop, W1) + b1)
 h1_drop = tf.nn.dropout(h1, keep_prob)
 
+# h1_drop
 W2 = tf.Variable(tf.truncated_normal([n_hidden, n_hidden], stddev=0.01))
 b2 = tf.Variable(tf.zeros([n_hidden]))
 h2 = tf.nn.relu(tf.matmul(h1_drop, W2) + b2)
 h2_drop = tf.nn.dropout(h2, keep_prob)
 
-# 隠れ層 - 出力層
+# 隠れ層 -> 出力層
 W3 = tf.Variable(tf.truncated_normal([n_hidden, n_out], stddev=0.01))
 b3 = tf.Variable(tf.zeros([n_out]))
 y = tf.nn.softmax(tf.matmul(h2_drop, W3) + b3)
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(t * tf.log(y), axis=1))
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+train_op = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(t, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -96,7 +83,7 @@ for epoch in range(epochs):
         start = i * batch_size
         end = start + batch_size
 
-        sess.run(train_step, feed_dict={
+        sess.run(train_op, feed_dict={
             x: X_[start:end],
             t: Y_[start:end],
             keep_prob: 0.5
