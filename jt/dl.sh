3a632edb1e73b57c7ec88f41557a41c9c33470df
Ruo Ando <ando.ruo@gmail.com>
2020-08-22 21:38:05 +0900

    jt

--- jt/dl.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ./mnist_data/mldata
-cd ./mnist_data/mldata
-wget https://github.com/amplab/datascience-sp14/raw/master/lab7/mldata/mnist-original.mat
-
-
-
--- jt/get_data_home.py
+++
@@ -1,2 +0,0 @@
-from sklearn.datasets.base import get_data_home
-print (get_data_home())
